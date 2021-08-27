5e1221cea24a543bbdf66f81f0381fc9a863d442
phamvancam2104 <phamvancam2104@gmail.com>
2021-08-28 02:06:30 +0700

    update script compile

--- deploy/1_game.js
+++ deploy/1_game.js
@@ -12,44 +12,7 @@ const {
   const _ = require('lodash');
   
   module.exports = async (hre) => {
-      const { ethers, upgrades, getNamedAccounts } = hre;
-      const { deployer, protocolOwner, trustedForwarder } = await getNamedAccounts();
-      const network = await hre.network;
-      const deployData = {};
-  
-      const chainId = chainIdByName(network.name);
-      const alchemyTimeout = chainId === 31337 ? 0 : (chainId === 1 ? 5 : 3);
-  
-      log('\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~');
-      log('DTO Multichain Bridge Protocol - Contract Deployment');
-      log('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n');
-  
-      log('  Using Network: ', chainNameById(chainId));
-      log('  Using Accounts:');
-      log('  - Deployer:          ', deployer);
-      log('  - network id:          ', chainId);
-      log('  - Owner:             ', protocolOwner);
-      log('  - Trusted Forwarder: ', trustedForwarder);
-      log(' ');
-  
-      log('  Deploying GenericBridge...');
-      const GenericBridge = await ethers.getContractFactory('GenericBridge');
-      const GenericBridgeInstance = await GenericBridge.deploy()
-      const genericBridge = await GenericBridgeInstance.deployed()
-      log('  - GenericBridge:         ', genericBridge.address);
-      deployData['GenericBridge'] = {
-        abi: getContractAbi('GenericBridge'),
-        address: genericBridge.address,
-        deployTransaction: genericBridge.deployTransaction,
-      }
-  
-      await genericBridge.setSupportedChainIds(supportedChainIds, true)
-      await genericBridge.addApprovers(approvers)
-  
-      saveDeploymentData(chainId, deployData);
-      log('\n  Contract Deployment Data saved to "deployments" directory.');
-  
-      log('\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n');
+     
   };
   
   module.exports.tags = ['game']
