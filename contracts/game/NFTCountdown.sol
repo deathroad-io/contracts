162fdc833ab49ad8cc8c1a89b0ef99f0803a6600
phamvancam2104 <phamvancam2104@gmail.com>
2021-09-22 00:28:27 +0700

    add countdown deployment

--- contracts/game/NFTCountdown.sol
+++ contracts/game/NFTCountdown.sol
@@ -1,12 +1,18 @@
 pragma solidity ^0.8.0;
 import "../interfaces/INFTFactory.sol";
 import "../interfaces/INFTCountdown.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract NFTCountdown is INFTCountdown {
+contract NFTCountdown is INFTCountdown, Ownable  {
+    uint256 public defaultCountdown;
     function getCountdownPeriod(uint256 _tokenId, address _nftFactory) external override view returns (uint256) {
         INFTFactory factory = INFTFactory(_nftFactory);
 
         //TODO: implement period based on token id
-        return 3600*2;
+        return defaultCountdown;
     } 
+
+    function setDefaultCountDown(uint256 _x) external onlyOwner {
+        defaultCountdown = _x;
+    }
 }
---
+++ deploy/13_countdown.js
@@ -0,0 +1,48 @@
+const {
+    chainNameById,
+    chainIdByName,
+    saveDeploymentData,
+    getContractAbi,
+    log
+  } = require("../js-helpers/deploy");
+  
+  const _ = require('lodash');
+  
+  module.exports = async (hre) => {
+    const { ethers, getNamedAccounts } = hre;
+    const { deployer } = await getNamedAccounts();
+    const network = await hre.network;
+    const deployData = {};
+
+    const signers = await ethers.getSigners()
+    const chainId = chainIdByName(network.name);
+
+    log('\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~');
+    log('DeathRoad NFTCountdown deployment');
+    log('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n');
+
+    log('  Using Network: ', chainNameById(chainId));
+    log('  Using Accounts:');
+    log('  - Deployer:          ', signers[0].address);
+    log('  - network id:          ', chainId);
+    log(' ');
+
+    log('  Deploying NFTCountdown...');
+    const NFTCountdown = await ethers.getContractFactory('NFTCountdown');
+    const NFTCountdownnstance = await NFTCountdown.deploy()
+    const countdown = await NFTCountdownnstance.deployed()
+    log('  - NFTCountdown:         ', countdown.address);
+    deployData['NFTCountdown'] = {
+      abi: getContractAbi('NFTCountdown'),
+      address: countdown.address,
+      deployTransaction: countdown.deployTransaction,
+    }
+
+    saveDeploymentData(chainId, deployData);
+    log('\n  Contract Deployment Data saved to "deployments" directory.');
+
+    log('\n~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~\n');
+  };
+  
+  module.exports.tags = ['countdown']
+  
